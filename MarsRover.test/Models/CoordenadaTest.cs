54dd1622cc3c2ea1126004da78d984725cf8db97
Ramón Carrasco <rncc@gft.com>
2023-05-23 17:38:16 +0200

    ADDED coordenada + unit tests

---
+++ MarsRover.test/Models/CoordenadaTest.cs
@@ -0,0 +1,122 @@
+﻿using MarsRover.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Test.Models
+{
+    public class CoordenadaTest
+    {
+        [Test]
+        public void Coordenada_X_EnConstructor_DebeSerMayorQue0()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => { Coordenada test = new Coordenada(0, 1); });
+        }
+
+        [Test]
+        public void Coordenada_X_EnConstructor_DebeSerPositivo()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => { Coordenada test = new Coordenada(-1, 1); });
+        }
+
+        [Test]
+        public void Coordenada_Y_EnConstructor_DebeSerMayorQue0()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => { Coordenada test = new Coordenada(1, 0); });
+        }
+
+        [Test]
+        public void Coordenada_Y_EnConstructor_DebeSerPositivo()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => { Coordenada test = new Coordenada(1, -1); });
+        }
+
+        [Test]
+        public void Coordenada_X_Asignada_DebeSerMayorQue0()
+        {
+            Coordenada test = new(1, 1);
+            Assert.Throws<ArgumentOutOfRangeException>(() => { test.X = 0; });
+        }
+
+        [Test]
+        public void Coordenada_X_Asignada_DebeSerPositiva()
+        {
+            Coordenada test = new(1, 1);
+            Assert.Throws<ArgumentOutOfRangeException>(() => { test.X = -1; });
+        }
+
+        [Test]
+        public void Coordenada_Y_Asignada_DebeSerMayorQue0()
+        {
+            Coordenada test = new(1, 1);
+            Assert.Throws<ArgumentOutOfRangeException>(() => { test.Y = 0; });
+        }
+
+        [Test]
+        public void Coordenada_Y_Asignada_DebeSerPositiva()
+        {
+            Coordenada test = new(1, 1);
+            Assert.Throws<ArgumentOutOfRangeException>(() => { test.Y = -1; });
+        }
+
+        [Test]
+        public void Coordenada_X_DebeDevolverElValorInicializado()
+        {
+            // Arrange
+            int initialValue = 1;
+
+            // Act
+            Coordenada test = new(initialValue, 1);
+            int x = test.X;
+
+            // Assert
+            Assert.That(x, Is.EqualTo(initialValue));
+        }
+
+        [Test]
+        public void Coordenada_Y_DebeDevolverElValorInicializado()
+        {
+            // Arrange
+            int initialValue = 1;
+
+            // Act
+            Coordenada test = new(1, initialValue);
+            int y = test.Y;
+
+            // Assert
+            Assert.That(y, Is.EqualTo(initialValue));
+        }
+
+        [Test]
+        public void Coordenada_X_DebeDevolverElValorSeteado()
+        {
+            // Arrange                        
+            int settedValue = 1;
+            Coordenada test = new(10, 10);
+
+            // Act
+            test.X = settedValue;
+            int x = test.X;
+
+            // Assert
+            Assert.That(x, Is.EqualTo(settedValue));
+        }
+
+        [Test]
+        public void Coordenada_Y_DebeDevolverElValorSeteado()
+        {
+            // Arrange            
+            int settedValue = 1;
+            Coordenada test = new(10, 10);
+
+            // Act
+            test.Y = settedValue;
+            int y = test.Y;
+
+            // Assert
+            Assert.That(y, Is.EqualTo(settedValue));
+        }
+    }
+}
---
+++ MarsRover/Models/Coordenada.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Models
+{
+    public class Coordenada
+    {
+        private int x;
+        private int y;
+
+        public int X 
+        { 
+            get 
+            { 
+                return x; 
+            } 
+            
+            set 
+            {
+                ValidateXvalue(value);
+                x = value; 
+            } 
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+
+            set
+            {
+                ValidateYvalue(value);
+                y = value;
+            }
+        }
+
+        public Coordenada(int x, int y)        {
+            ValidateXvalue(x);
+            ValidateYvalue(y);
+            this.x = x;
+            this.y = y;
+        }
+
+        private static void ValidateXvalue(int x)
+        {
+            if (x <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(x), "El valor de X debe ser un número mayor que 0");
+            }
+        }
+
+        private static void ValidateYvalue(int y)
+        {
+            if (y <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(y), "El valor de Y debe ser un número mayor que 0");
+            }
+        }
+    }
+}
