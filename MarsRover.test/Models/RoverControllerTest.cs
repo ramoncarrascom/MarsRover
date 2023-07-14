34a0985e54b9e540ac1ab692a9572b3b314caab9
Ramón Carrasco <rncc@gft.com>
2023-07-14 13:59:02 +0200

    ADDED RoverController + Tests

---
+++ MarsRover.test/Models/RoverControllerTest.cs
@@ -0,0 +1,90 @@
+﻿using MarsRover.Enums;
+using MarsRover.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Test.Models
+{
+    public class RoverControllerTest
+    {
+        [Test]
+        public void RoverController_DebeMoverRover_HaciaAdelante_ConOrdenM()
+        {
+            // Arrange
+            Cuadricula cuadricula = new(5, 5);
+            Coordenada inicio = new(3, 3);
+            Orientacion orientacionInicial = new(Orientaciones.NORTE);
+            Rover rover = new(cuadricula, inicio, orientacionInicial);
+            Instrucciones instrucciones = new("M");
+            RoverController controller = new(rover, instrucciones);
+
+            // Act
+            controller.EjecutarInstrucciones();
+
+            // Assert
+            Assert.That(rover.Coordenadas.Y, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void RoverController_DebeGirarRoverALaIzquierda_ConOrdenL()
+        {
+            // Arrange
+            Cuadricula cuadricula = new(5, 5);
+            Coordenada inicio = new(3, 3);
+            Orientacion orientacionInicial = new(Orientaciones.NORTE);
+            Rover rover = new(cuadricula, inicio, orientacionInicial);
+            Instrucciones instrucciones = new("L");
+            RoverController controller = new(rover, instrucciones);
+
+            // Act
+            controller.EjecutarInstrucciones();
+
+            // Assert
+            Assert.That(rover.Orientacion.Valor, Is.EqualTo(Orientaciones.OESTE));
+        }
+
+        [Test]
+        public void RoverController_DebeGirarRoverALaDerecha_ConOrdenR()
+        {
+            // Arrange
+            Cuadricula cuadricula = new(5, 5);
+            Coordenada inicio = new(3, 3);
+            Orientacion orientacionInicial = new(Orientaciones.NORTE);
+            Rover rover = new(cuadricula, inicio, orientacionInicial);
+            Instrucciones instrucciones = new("R");
+            RoverController controller = new(rover, instrucciones);
+
+            // Act
+            controller.EjecutarInstrucciones();
+
+            // Assert
+            Assert.That(rover.Orientacion.Valor, Is.EqualTo(Orientaciones.ESTE));
+        }
+
+        [Test]
+        public void RoverController_DebeProcesarUnaSecuenciaDeOrdenes()
+        {
+            // Arrange
+            Cuadricula cuadricula = new(5, 5);
+            Coordenada inicio = new(3, 3);
+            Orientacion orientacionInicial = new(Orientaciones.NORTE);
+            Rover rover = new(cuadricula, inicio, orientacionInicial);
+            Instrucciones instrucciones = new("MLMR");
+            RoverController controller = new(rover, instrucciones);
+
+            // Act
+            controller.EjecutarInstrucciones();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(rover.Coordenadas.X, Is.EqualTo(2));
+                Assert.That(rover.Coordenadas.Y, Is.EqualTo(2));
+                Assert.That(rover.Orientacion.Valor, Is.EqualTo(Orientaciones.NORTE));
+            });
+        }
+    }
+}
---
+++ MarsRover/Models/RoverController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Models
+{
+    public class RoverController
+    {
+        private readonly Rover rover;
+        private readonly Instrucciones instrucciones;
+
+        public RoverController(Rover rover, Instrucciones instrucciones)
+        {
+            this.rover = rover;
+            this.instrucciones = instrucciones;
+        }
+
+        public void EjecutarInstrucciones()
+        {
+            char? orden;
+            while ((orden = instrucciones.SiguienteOrden()) != null)
+            {
+                switch (orden)
+                {
+                    case 'M':
+                        rover.Avanzar();
+                        break;
+                    case 'L':
+                        rover.GirarIzquierda();
+                        break;
+                    case 'R':
+                        rover.GirarDerecha();
+                        break;
+                    default:
+                        throw new InvalidOperationException($"Orden desconocida: {orden}");
+                }
+            }
+        }
+    }
+}
